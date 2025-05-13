1af619f65e4d83cfa1e49a3684f79c817a319f5e
Ellie <19760814+ellzie@users.noreply.github.com>
2025-05-12 20:17:52 -0400

    fixed subtype not being properly deserialized

--- src/main/java/me/bed0/jWynn/api/v3/item/WynncraftItem.java
+++ src/main/java/me/bed0/jWynn/api/v3/item/WynncraftItem.java
@@ -1,5 +1,6 @@
 package me.bed0.jWynn.api.v3.item;
 
+import com.google.gson.annotations.SerializedName;
 import me.bed0.jWynn.api.v3.item.icon.WynncraftIcon;
 import me.bed0.jWynn.api.v3.item.stat.WynncraftStat;
 
@@ -9,14 +10,17 @@ import java.util.HashMap;
 public class WynncraftItem {
     private String internalName;
     private String type;
+
+    @SerializedName(value = "weaponType", alternate = {"armourType", "accessoryType","tomeType", "charmType", "toolType", "ingredientType", "materialType"})
     private String subtype;
+
     private WynncraftIcon icon;
     private boolean identifier;
     private boolean allow_craftsman;
 
     private String armourMaterial;
     private String attackSpeed;
-    private int averageDPS;
+    private int averageDps;
     private int gatheringSpeed;
     private String tier;
     private String rarity;
@@ -33,6 +37,33 @@ public class WynncraftItem {
     private WynncraftItemRequirements requirements;
     private HashMap<String, WynncraftStat> identifications;
 
+    public WynncraftItem(String internalName, String type, String subtype, WynncraftIcon icon, boolean identifier, boolean allow_craftsman, String armourMaterial, String attackSpeed, int averageDPS, int gatheringSpeed, String tier, String rarity, WynncraftConsumableIDs consumableOnlyIDs, WynncraftIngredientIDs itemOnlyIDs, HashMap<String, String> majorIds, String[] craftable, int powderSlots, String lore, String dropRestriction, boolean raidReward, WynncraftDropLocation dropMeta, HashMap<String, WynncraftStat> base, WynncraftItemRequirements requirements, HashMap<String, WynncraftStat> identifications) {
+        this.internalName = internalName;
+        this.type = type;
+        this.subtype = subtype;
+        this.icon = icon;
+        this.identifier = identifier;
+        this.allow_craftsman = allow_craftsman;
+        this.armourMaterial = armourMaterial;
+        this.attackSpeed = attackSpeed;
+        this.averageDps = averageDPS;
+        this.gatheringSpeed = gatheringSpeed;
+        this.tier = tier;
+        this.rarity = rarity;
+        this.consumableOnlyIDs = consumableOnlyIDs;
+        this.itemOnlyIDs = itemOnlyIDs;
+        this.majorIds = majorIds;
+        this.craftable = craftable;
+        this.powderSlots = powderSlots;
+        this.lore = lore;
+        this.dropRestriction = dropRestriction;
+        this.raidReward = raidReward;
+        this.dropMeta = dropMeta;
+        this.base = base;
+        this.requirements = requirements;
+        this.identifications = identifications;
+    }
+
     @Override
     public String toString() {
         return "WynncraftItem{" +
@@ -44,7 +75,7 @@ public class WynncraftItem {
                 ", allow_craftsman=" + allow_craftsman +
                 ", armourMaterial='" + armourMaterial + '\'' +
                 ", attackSpeed='" + attackSpeed + '\'' +
-                ", averageDPS=" + averageDPS +
+                ", averageDPS=" + averageDps +
                 ", gatheringSpeed=" + gatheringSpeed +
                 ", tier='" + tier + '\'' +
                 ", rarity='" + rarity + '\'' +
